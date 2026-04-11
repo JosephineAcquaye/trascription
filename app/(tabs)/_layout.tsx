1bc74fc20e6b2e706dec177c8365345db9b8e690
JosephineAcquaye <pennysummers01@gmail.com>
2026-04-11 22:49:42 +0000

    feat:clerk auth implementation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,8 @@
 import { tabs } from "@/constants/data";
 import { colors, components } from "@/constants/theme";
+import { useAuth } from "@clerk/expo";
 import clsx from "clsx";
-import { Tabs } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 import { Image, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
@@ -18,8 +19,17 @@ const TabIcon = ({ focused, icon }: TabIconProps) => {
 };
 const TabLayout = () => {
   const insets = useSafeAreaInsets();
+  const { isSignedIn, isLoaded } = useAuth();
 
-  // Wait for auth to load before rendering anything
+  // Wait for auth to load before rendering
+  if (!isLoaded) {
+    return null;
+  }
+
+  // If not signed in, redirect to sign-in
+  if (!isSignedIn) {
+    return <Redirect href="/(auth)/sign-in" />;
+  }
 
   return (
     <Tabs
