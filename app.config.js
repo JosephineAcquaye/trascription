79738ee31398e545146fd947ce0f482abbeff83e
JosephineAcquaye <pennysummers01@gmail.com>
2026-04-20 12:24:16 +0000

    update

--- app.config.js
+++ app.config.js
@@ -1,67 +1,20 @@
+const appJson = require("./app.json");
+
 export default {
   expo: {
-    name: "Trascription",
-    slug: "Trascription",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "trascription",
-    userInterfaceStyle: "automatic",
-    newArchEnabled: true,
-    ios: {
-      supportsTablet: true,
-    },
-    android: {
-      adaptiveIcon: {
-        backgroundColor: "#E6F4FE",
-        foregroundImage: "./assets/images/android-icon-foreground.png",
-        backgroundImage: "./assets/images/android-icon-background.png",
-        monochromeImage: "./assets/images/android-icon-monochrome.png",
-      },
-      edgeToEdgeEnabled: true,
-      predictiveBackGestureEnabled: false,
-    },
-    web: {
-      output: "static",
-      favicon: "./assets/images/favicon.png",
-    },
-    plugins: [
-      "expo-router",
-      [
-        "expo-splash-screen",
-        {
-          image: "./assets/images/splash-icon.png",
-          imageWidth: 200,
-          resizeMode: "contain",
-          backgroundColor: "#ffffff",
-          dark: {
-            backgroundColor: "#000000",
-          },
-        },
-      ],
-      [
-        "expo-font",
-        {
-          fonts: [
-            "./assets/fonts/PlusJakartaSans-Regular.ttf",
-            "./assets/fonts/PlusJakartaSans-Bold.ttf",
-            "./assets/fonts/PlusJakartaSans-Medium.ttf",
-            "./assets/fonts/PlusJakartaSans-SemiBold.ttf",
-            "./assets/fonts/PlusJakartaSans-ExtraBold.ttf",
-            "./assets/fonts/PlusJakartaSans-Light.ttf",
-          ],
-        },
-      ],
-      "@clerk/expo",
-      "expo-secure-store",
-    ],
+    ...appJson.expo,
     extra: {
+      ...(appJson.expo?.extra || {}),
       posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
       posthogHost: process.env.POSTHOG_HOST,
+      eas: {
+        projectId: "a4565ac3-30a5-4cfd-a10a-cf6db304ed7d",
+      },
     },
-    experiments: {
-      typedRoutes: true,
-      reactCompiler: true,
+  },
+  ios: {
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false,
     },
   },
 };
